0e611343cdabdf5a576561d50af729f7340106e3
Kasper Stevnbak <kasper@stevnbak.dk>
2025-10-24 10:41:31 +0200

    Environment variables

--- src/main/java/com/group6/SongQueue/view/Home.java
+++ src/main/java/com/group6/SongQueue/view/Home.java
@@ -1,13 +1,18 @@
 package com.group6.SongQueue.view;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Home {
+
+    @Value("${test:#{null}}")
+    private String testEnv;
     
     @GetMapping(path="/")
     public String getHomeView() {
+        System.out.println(testEnv);
         return "home";
     }
 
